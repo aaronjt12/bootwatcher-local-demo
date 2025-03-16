93d4642c09a196b43d54c27bf8e042bd0051122a
Aaron Taylor <aaronjtaylor45@gmail.com>
2025-03-16 17:32:11 -0400

    sends sms to every number 2

--- starter/vite.config.js
+++ starter/vite.config.js
@@ -14,11 +14,20 @@
  * limitations under the License.
 */
 import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({mode}) => {
   const {GOOGLE_MAPS_API_KEY = ''} = loadEnv(mode, process.cwd(), '');
 
   return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+      commonjsOptions: {
+        include: [/node_modules/],
+      },
+    },
     define: {
       'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(GOOGLE_MAPS_API_KEY)
     },
