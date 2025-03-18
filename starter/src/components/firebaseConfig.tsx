8a138ba0ba4e889ad27ac6015d07fa2c25a6669b
Aaron Taylor <aaronjtaylor45@gmail.com>
2025-03-18 12:00:23 -0400

    firebase fix

--- starter/src/components/firebaseConfig.tsx
+++ starter/src/components/firebaseConfig.tsx
@@ -7,13 +7,13 @@ const getEnv = (key: string): string => {
 
 // Use environment variables for Firebase configuration
 const firebaseConfig = {
-  apiKey: getEnv('VITE_FIREBASE_API_KEY'),
-  authDomain: getEnv('VITE_FIREBASE_AUTH_DOMAIN') || "bootwatcher-demo.firebaseapp.com",
-  databaseURL: getEnv('VITE_FIREBASE_DATABASE_URL') || "https://bootwatcher-demo-default-rtdb.firebaseio.com",
-  projectId: getEnv('VITE_FIREBASE_PROJECT_ID') || "bootwatcher-demo",
-  storageBucket: getEnv('VITE_FIREBASE_STORAGE_BUCKET') || "bootwatcher-demo.appspot.com",
-  messagingSenderId: getEnv('VITE_FIREBASE_MESSAGING_SENDER_ID') || "123456789012",
-  appId: getEnv('VITE_FIREBASE_APP_ID') || "1:123456789012:web:abc123def456",
+  apiKey: getEnv('VITE_FIREBASE_API_KEY'),rtdb
+  authDomain: getEnv('VITE_FIREBASE_AUTH_DOMAIN'),
+  databaseURL: getEnv('VITE_FIREBASE_DATABASE_URL'),
+  projectId: getEnv('VITE_FIREBASE_PROJECT_ID'),
+  storageBucket: getEnv('VITE_FIREBASE_STORAGE_BUCKET'),
+  messagingSenderId: getEnv('VITE_FIREBASE_MESSAGING_SENDER_ID'),
+  appId: getEnv('VITE_FIREBASE_APP_ID'),
 };
 
 console.log('Firebase config:', firebaseConfig);
