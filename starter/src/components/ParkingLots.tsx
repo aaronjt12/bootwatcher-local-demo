1651bce7c8b2756b9e56b7f86c63220ef6b5497f
Aaron Taylor <aaronjtaylor45@gmail.com>
2025-03-18 13:19:57 -0400

    google fix

--- starter/src/components/ParkingLots.tsx
+++ starter/src/components/ParkingLots.tsx
@@ -22,11 +22,12 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_DATABASE_URL,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  projectId: 'bootwatcher-82f5d',
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
